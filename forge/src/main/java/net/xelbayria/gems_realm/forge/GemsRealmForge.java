ab52026f8cf7e91f9a607fbb3c57ac7f7690f8e9
Xel'Bayria <12866666+Xelbayria@users.noreply.github.com>
2025-05-26 00:17:53 -0600

    GemsRealmForge: Quick Cleanup, Reorganized, Enabled 3 mods

--- forge/src/main/java/net/xelbayria/gems_realm/forge/GemsRealmForge.java
+++ forge/src/main/java/net/xelbayria/gems_realm/forge/GemsRealmForge.java
@@ -1,8 +1,13 @@
 package net.xelbayria.gems_realm.forge;
 
+import net.minecraftforge.fml.common.Mod;
 import net.xelbayria.gems_realm.GemsRealm;
 import net.xelbayria.gems_realm.GemsRealmCommon;
-import net.minecraftforge.fml.common.Mod;
+import net.xelbayria.gems_realm.modules.forge.macaws.MacawBridgesModule;
+import net.xelbayria.gems_realm.modules.forge.macaws.MacawFencesModule;
+import net.xelbayria.gems_realm.modules.forge.macaws.MacawWindowsModule;
+
+import static net.mehvahdjukaar.every_compat.EveryCompat.addIfLoaded;
 
 /**
  * Author: Xel'Bayria
@@ -21,19 +26,18 @@ public class GemsRealmForge extends GemsRealmCommon {
 //!! =================================================== Add Modules ================================================ \\
 
         // Macaw's
-//        addIfLoaded("mcwbridges", () -> MacawBridgesModule::new);
-//        addIfLoaded("mcwfences", () -> MacawFencesModule::new);
-//        addIfLoaded("mcwwindows", () -> MacawWindowsModule::new);
+        addIfLoaded("mcwbridges", () -> MacawBridgesModule::new);
+        addIfLoaded("mcwwindows", () -> MacawWindowsModule::new);
+        addIfLoaded("mcwfences", () -> MacawFencesModule::new);
 //        addIfLoaded("mcwroofs", () -> MacawRoofsModule::new);
 //        addIfLoaded("mcwstairs", () -> MacawStairsModule::new);
 //        addIfLoaded("mcwpaths", () -> MacawPathsModule::new);
 
         // General
+//        addIfLoaded("create", () -> CreateModule::new);
 //        addIfLoaded("rechiseled", () -> RechiseledModule::new);
 //        addIfLoaded("stonechest", () -> StoneChestModule::new);
 
-        //!! TEMP - Until Fabric version is updated to v6.0.0
-//        addIfLoaded("create", () -> CreateModule::new);
 
 //!! ====================================================== OTHERS ================================================== \\
 
