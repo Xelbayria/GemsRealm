8e6c691b1f998856792fd106cc620af4aae290c6
Xel'Bayria <12866666+Xelbayria@users.noreply.github.com>
2025-05-31 11:57:38 -0600

    SpriteHelper: Updated & Cleanup

--- common/src/main/java/net/xelbayria/gems_realm/misc/SpriteHelper.java
+++ common/src/main/java/net/xelbayria/gems_realm/misc/SpriteHelper.java
@@ -2,9 +2,9 @@ package net.xelbayria.gems_realm.misc;
 
 
 import net.mehvahdjukaar.moonlight.api.client.TextureCache;
-import net.xelbayria.gems_realm.GemsRealm;
 import net.minecraft.core.registries.BuiltInRegistries;
 import net.minecraft.resources.ResourceLocation;
+import net.xelbayria.gems_realm.GemsRealm;
 
 import java.util.HashMap;
 import java.util.Map;
@@ -15,33 +15,24 @@ public class SpriteHelper {
 
     public static void addHardcodedSprites() {
 
-                //!! MODELS !!\\
-/// NOTE: Used to identify StoneTypes' model only based off their name or path
-        // Naturalist
-        addToModelId("bbb/naturalist/shellstone_layer", "naturalist:block/shellstone/shellstone");
-        addToModelId("bbb/naturalist/shellstone_brick_layer", "naturalist:block/shellstone/shellstone_bricks");
-        addToModelId("bbb/naturalist/smooth_shellstone_layer", "naturalist:block/shellstone/smooth_shellstone");
+                ///  MODELS  \\\
+// NOTE: Used to identify StoneTypes' model only based off their name or path
 
-        // BetterEnd
-        addToModelId("bbb/betterend/umbralith_layer", "betterend:block/umbralith_1");
 
-        // Eternal Tales
-        addToModelId("bbb/eternal_tales/smooth_purgatorium_stone_layer", "eternal_tales:block/purgatorium_stone_smooth");
-        addToModelId("bbb/eternal_tales/smooth_rajiit_layer", "eternal_tales:block/rajiit_smooth");
-
-                //!! TEXTURES !!\\
-/// NOTE: Used to identify StoneTypes' texture only based off their name
-        // BetterEnd
-        addOptional("betterend:umbralith", "all", "betterend:block/umbralith");
-
-        // Eternal Tales
-        addOptional("eternal_tales:purgatorium_stone", "all", "eternal_tales:block/purgstone");
+                /// TEXTURES \\\
+// NOTE: Used to identify BlockTypes' texture only based off their name and path
     }
 
     @SuppressWarnings("SameParameterValue")
-    private static void addOptional(String blockId, String textureId, String texturePath) {
+    private static void addOptionalBlock(String blockId, String textureId, String texturePath) {
         BuiltInRegistries.BLOCK.getOptional(new ResourceLocation(blockId))
-                .ifPresent(b -> TextureCache.registerSpecialTextureForBlock(b, textureId, new ResourceLocation(texturePath)));
+                .ifPresent(block -> TextureCache.registerSpecialTextureForBlock(block, textureId, new ResourceLocation(texturePath)));
+    }
+
+    @SuppressWarnings("SameParameterValue")
+    private static void addOptionalItem(String itemId, String textureId, String texturePath) {
+        BuiltInRegistries.ITEM.getOptional(new ResourceLocation(itemId))
+                .ifPresent(item -> TextureCache.registerSpecialTextureForBlock(item, textureId, new ResourceLocation(texturePath)));
     }
 
     private static void addToModelId(String blockid, String pathModel) {
