934bab4f87066426e41871e91dbd3871ab4d5c81
Xel'Bayria <12866666+Xelbayria@users.noreply.github.com>
2025-06-09 01:11:08 -0600

    RockType: Updated & Improved

--- common/src/main/java/net/xelbayria/gems_realm/api/set/RockType.java
+++ common/src/main/java/net/xelbayria/gems_realm/api/set/RockType.java
@@ -9,102 +9,20 @@ import net.minecraft.world.level.ItemLike;
 import net.minecraft.world.level.block.Block;
 import org.jetbrains.annotations.Nullable;
 
+import java.util.Objects;
+
+/**
+ * Childkey Availability:
+ * block, raw_block,
+ * stairs, slab, fence,
+ * chiseled, bricks, brick_stairs, brick_slab, brick_wall, brick_tiles,
+ * cracked_bricks, mossy_bricks, mossy_brick_slab, mossy_brick_stairs, mossy_brick_wall,
+ * smooth, smooth_stairs, smooth_slab, smooth_wall
+**/
+@SuppressWarnings("SameParameterValue")
 public abstract class RockType extends BlockType{
-    /**
-     * Childkey Availability:
-     **/
-
-    /**
-     * CrystalCraft:
-     *  REDSTONE_DUST
-     * crystalcraft_unlimited_java:bluestone_dust - ITEM - detected by #forge:dusts/...
-     *
-     *      INGOT:
-     * crystalcraft_unlimited_java:magnesium_ore - BLOCK - detected by BASS_DRUM
-     * crystalcraft_unlimited_java:magnesium_block - BLOCK - detected by BASS_DRUM
-     * crystalcraft_unlimited_java:raw_magnesium_block - BLOCK - detected by HARP
-     * crystalcraft_unlimited_java:raw_magnesium - ITEM
-     * crystalcraft_unlimited_java:magnesium - ITEM (INGOT) - detected by #forge:ingots
-     *
-     *      GEM:
-     *  crystalcraft_unlimited_java:garnet_ore - BLOCK - detected by BASS_DRUM
-     *  crystalcraft_unlimited_java:garnet_block - BLOCK - detected by BASS_DRUM
-     *  crystalcraft_unlimited_java:garnet - ITEM (GEM) - detected by #forge:gems
-     *
-     *  crystalcraft_unlimited_java:amethyst_ore - BLOCK - detected by BASS_DRUM
-     *  crystalcraft_unlimited_java:amethyst_block - BLOCK - detected by BASS_DRUM
-     *  crystalcraft_unlimited_java:amethyst - ITEM (GEM)
-     *
-     *  crystalcraft_unlimited_java:fire_opal_ore - BLOCK - detected by BASS_DRUM
-     *  crystalcraft_unlimited_java:fire_opal_block - BLOCK - detected by HARP
-     *  crystalcraft_unlimited_java:fire_opal - ITEM (GEM)
-     *
-     * BetterEnd
-     *      GEM:
-     *  betterend:amber_gem - ITEM (GEM) - ???
-     *  betterend:raw_amber - ITEM
-     *  betterend:amber_block - BLOCK - detected by HARP
-     *  betterend:amber_ore - BLOCK - detected by BASS_DRUM
-     *
-     *      INGOT:
-     *  betterend:thallasium_block - BLOCK - detected by VIBRAPHONE
-     *  betterend:thallasium_ingot - ITEM
-     *  betterend:thallasium_nugget - ITEM
-     *
-     *  betterend:terminite_ingot - ITEM
-     *  betterend:terminite_block - BLOCK - detected by VIBRAPHONE
-     *  betterend:terminite_nugget - ITEM
-     *
-     *  betterend:aeternium_block - BLOCK - detected by VIBRAPHONE
-     *  betterend:aeternium_ingot - ITEM
-     *
-     * TFC:
-     *      INGOT:
-     * tfc:ore/poor_bismuth/granite
-     * tfc:metal/ingot/bismuth
-     * tfc:metal/block/bismuth
-
-     *      GEM:
-     * tfc:ore/amethyst - ITEM
-     * tfc:gem/amethyst - ITEM - detected by #forge:gems
-     *
-     * Galosphere
-     *      CRYSTAL:
-     * galosphere:amethyst_cluster - BLOCK - detected by HARP
-     * galosphere:amethyst_block - BLOCK - detected by HARP
-     *
-     * galosphere:allurite_cluster - BLOCK - detected by HARP
-     * galosphere:allurite_block - BLOCK - detected by HARP
-     *
-     * Mo' Shiz
-     * ms:gem/cobalt_ingot - ITEM
-     * ms:resources/cobalt_block - BLOCK - detected by HARP
-     * ms:gem/raw_cobalt - ITEM
-     * ms:nugget/cobalt_nugget - ITEM
-     *
-     * ms:fence/cobalt - BLOCK
-     * ms:stair/cobalt - BLOCK
-     * ms:slab/cobalt - BLOCK
-     * ms:vertical/cobalt - BLOCK
-     *
-     * ms:gem/sapphire - ITEM (GEM)
-     * ms:resources/sapphire_block - BLOCK
-     * ms:nugget/sapphire_shard - ITEM (RAW)
-     * ms:fence/sapphirefence
-     * ms:stair/sapphirestair
-     * ms:slab/sapphire_slab
-     * ms:vertical/sapphire
-     *
-     * TOOLTIP - disable it
-     *         ItemStack stack = event.getItemStack();
-     *         if (stack != null) {
-     *             // Remove tags from the tooltip
-     *             event.getTooltipElements().removeIf(element -> element instanceof TooltipComponent && element.getText().getString().contains("#"));
-     *         }
-     */
 
     public final Block block;
-//    public final Block ingot;
 
     protected RockType(ResourceLocation id, Block block) {
         super(id);
@@ -112,33 +30,63 @@ public abstract class RockType extends BlockType{
     }
 
     @Override
-    public String getAppendableIdWith(String prefix, String suffix) {
-        String suffixed = (suffix.isEmpty()) ? "" : "_" + suffix;
-        String prefixed = (prefix.isEmpty()) ? "" : prefix + "_";
-        return  this.getNamespace() +"/"+ prefixed + this.getTypeName() + suffixed;
-    }
-
-    @Override
     protected void initializeChildrenBlocks() {
-            this.addChild("block", this.block);
-            this.addChild("raw_block", findRelatedBlock("raw", "block"));
+        this.addChild("block", this.block);
+        this.addChild("raw_block", findRelatedBlock("raw", "block"));
+        this.addChild("stairs", findChildBlocks("stair"));
+        this.addChild("slab", findChildBlocks("slab"));
+        this.addChild("fence", findChildBlocks("fence"));
+        this.addChild("chiseled", findRelatedBlock("chiseled", ""));
 
-            // Support TFC & AFC
-            if (this.id.getNamespace().matches("tfc|afc")) {
-                this.addChild("trapdoor", findRelatedBlock("trapdoor", ""));
-                this.addChild("lamp", findRelatedBlock("lamp", ""));
-                this.addChild("chain", findRelatedBlock("chain", ""));
-                this.addChild("anvil", findRelatedBlock("anvil", ""));
-                this.addChild("bars", findRelatedBlock("bars", ""));
-            }
-            else {
-            }
+        Block bricks = this.findRelatedEntry("bricks", BuiltInRegistries.BLOCK);
+        this.addChild("bricks", bricks);
+        if (bricks != null) {
+            this.addChild("brick_stairs", findChildBricks( "stairs"));
+            this.addChild("brick_slab", findChildBricks( "slab"));
+            this.addChild("brick_wall", findChildBricks( "wall"));
+            this.addChild("brick_tiles", findChildBricks( "tiles"));
+            this.addChild("cracked_bricks", findRelatedBrick("cracked", ""));
+            this.addChild("mossy_bricks", findRelatedBrick("mossy", ""));
+            this.addChild("mossy_brick_slab", findRelatedBrick("mossy", "slab"));
+            this.addChild("mossy_brick_stairs", findRelatedBrick("mossy", "stairs"));
+            this.addChild("mossy_brick_wall", findRelatedBrick("mossy", "wall"));
+        }
+
+        Block smooth = findRelatedEntry("smooth", BuiltInRegistries.BLOCK);
+        this.addChild("smooth", smooth);
+        if (Objects.nonNull(smooth)) {
+            this.addChild("smooth_stairs", findRelatedBlock("smooth", "stairs"));
+            this.addChild("smooth_slab", findRelatedBlock("smooth", "slab"));
+            this.addChild("smooth_wall", findRelatedBlock("smooth", "wall"));
+        }
 
     }
 
     @Override
     protected void initializeChildrenItems() {
-//        this.addChild("ingot", this.findRelatedEntry("ingot", BuiltInRegistries.ITEM));
+        this.addChild("ingot", findRelatedItem("", "ingot"));
+    }
+
+    /// Check for block with "s" and without "s"
+    private @Nullable Block findChildBlocks(String suffix) {
+        var first = this.findRelatedEntry("", suffix, BuiltInRegistries.BLOCK);
+        if (first != null) return first;
+        return this.findRelatedEntry("",suffix + "s", BuiltInRegistries.BLOCK);
+    }
+
+    /// Concatenation: "brick_" + suffix
+    private @Nullable Block findChildBricks(String suffix) {
+        var first = this.findRelatedEntry("brick_" + suffix, BuiltInRegistries.BLOCK);
+        if (first != null) return first;
+        return this.findRelatedEntry("bricks_" + suffix, BuiltInRegistries.BLOCK);
+    }
+    /// Concatentation: prefix + "_brick" + "_" + suffix
+    private @Nullable Block findRelatedBrick(String prefix, String suffix) {
+        suffix = (suffix.isEmpty()) ? "" : "_" + suffix;
+
+        var first = this.findRelatedEntry(prefix,"brick" + suffix, BuiltInRegistries.BLOCK);
+        if (first != null) return first;
+        return this.findRelatedEntry(prefix,"bricks" + suffix, BuiltInRegistries.BLOCK);
     }
 
     protected @Nullable Block findRelatedBlock(String prefixOrInfix, String suffix) {
@@ -151,13 +99,28 @@ public abstract class RockType extends BlockType{
 
     @Override
     protected @Nullable <V> V findRelatedEntry(String prefixOrInfix, String suffix, Registry<V> reg) {
-
-
+        String prefix = (prefixOrInfix.isEmpty()) ? "" : prefixOrInfix + "_";
+        String infix = (prefixOrInfix.isEmpty()) ? "" : "_" + prefixOrInfix;
+        String withoutUnderscore = suffix;
         if (!suffix.isEmpty()) suffix = "_" + suffix;
+
         ResourceLocation[] targets = {
-                new ResourceLocation(id.getNamespace(), id.getPath() +"_"+ prefixOrInfix + suffix),
-                new ResourceLocation(id.getNamespace(), prefixOrInfix +"_"+ id.getPath() + suffix),
+                // DEFAULT
+                new ResourceLocation(id.getNamespace(), id.getPath() + infix + suffix),
+                new ResourceLocation(id.getNamespace(), prefix + id.getPath() + suffix),
+
+                // Mo' Shiz: Include children: ingot, gem
+                new ResourceLocation(id.getNamespace(), "gem/"+ id.getPath() + suffix),
+                // Mo' Shiz: Include children: block, raw_block
+                new ResourceLocation(id.getNamespace(), "resources/"+ prefix + id.getPath() + suffix),
+                // Mo' Shiz: Include children: fence, stairs, slab
+                new ResourceLocation(id.getNamespace(), withoutUnderscore +"/"+ id.getPath() + suffix),
+                // Mo' Shiz: Include children: fence, stairs, slab (Another Naming Scheme)
+                new ResourceLocation(id.getNamespace(), withoutUnderscore +"/"+ id.getPath() + withoutUnderscore),
+                // Mo' Shiz: Include children: fence, stairs, slab (Another Naming Scheme)
+                new ResourceLocation(id.getNamespace(), withoutUnderscore +"/"+ id.getPath())
         };
+
         V found = null;
         for (var r : targets) {
             if (reg.containsKey(r)) {
@@ -169,18 +132,8 @@ public abstract class RockType extends BlockType{
     }
 
     @Override
-    protected <V> V findRelatedEntry(String prefixOrInfix, Registry<V> reg) {
-        return findRelatedEntry(prefixOrInfix, "", reg);
-    }
-
-    @Override
     public ItemLike mainChild() {
         return block;
-    }
-
-    public Block bricksOrStone() {
-        Block bricks= this.getBlockOfThis("bricks");
-        return bricks != null ? bricks : this.block;
     }
 
 }
