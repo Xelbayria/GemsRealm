4aa43cf839ac88e77e894c81a90e42de98de5131
Xel'Bayria <12866666+Xelbayria@users.noreply.github.com>
2025-06-16 23:43:20 -0600

    NEW MODULE: Create (FB) - Unfinished & Waiting

---
+++ fabric/src/main/java/net/xelbayria/gems_realm/modules/fabric/create/CreateClientModule.java
@@ -0,0 +1,83 @@
+package net.xelbayria.gems_realm.modules.fabric.create;
+
+import com.simibubi.create.CreateClient;
+import com.simibubi.create.content.decoration.MetalScaffoldingCTBehaviour;
+import com.simibubi.create.foundation.block.connected.*;
+import net.fabricmc.api.EnvType;
+import net.fabricmc.api.Environment;
+import net.mehvahdjukaar.every_compat.api.SimpleEntrySet;
+import net.mehvahdjukaar.moonlight.api.util.Utils;
+import net.minecraft.resources.ResourceLocation;
+import net.minecraft.world.level.block.Block;
+import net.xelbayria.gems_realm.GemsRealm;
+import net.xelbayria.gems_realm.api.GemsRealmModule;
+import net.xelbayria.gems_realm.api.set.MetalType;
+
+import static com.simibubi.create.foundation.block.connected.CTSpriteShifter.getCT;
+
+@Environment(EnvType.CLIENT)
+public class CreateClientModule {
+
+    public static void registerWindowCTBehavior(GemsRealmModule module, SimpleEntrySet<MetalType, Block> window, SimpleEntrySet<MetalType, Block> window_pane) {
+        window.blocks.forEach((metalType, block) -> {
+            String blockTexture = metalType.createFullIdWith(GemsRealm.MOD_ID, "block", module.shortenedId(), "palettes/ornate_", "window");
+
+            CTSpriteShiftEntry spriteShift = vertical(blockTexture, blockTexture + "_connected");
+
+            CreateClient.MODEL_SWAPPER.getCustomBlockModels().register(Utils.getID(block),
+                    model -> new CTModel(model, new HorizontalCTBehaviour(spriteShift)));
+            CreateClient.MODEL_SWAPPER.getCustomBlockModels().register(Utils.getID(window_pane.blocks.get(metalType)),
+                    model -> new CTModel(model, new GlassPaneCTBehaviour(spriteShift)));
+        });
+    }
+
+    public static void registerCasingCTBehavior(GemsRealmModule module, SimpleEntrySet<MetalType, Block> casing) {
+        casing.blocks.forEach((metalType, block) -> {
+            String blockTexture = metalType.createFullIdWith(GemsRealm.MOD_ID, "block", module.shortenedId(), "", "casing");
+            String connectedTexture = metalType.createFullIdWith(GemsRealm.MOD_ID, "block", module.shortenedId(), "", "casing_connected");
+
+            CTSpriteShiftEntry spriteShift = omni(blockTexture, connectedTexture);
+
+            CreateClient.MODEL_SWAPPER.getCustomBlockModels().register(Utils.getID(block),
+                    model -> new CTModel(model, new SimpleCTBehaviour(spriteShift)));
+
+        });
+    }
+
+    public static void registerScaffoldCTBehavior(GemsRealmModule module, SimpleEntrySet<MetalType, Block> scaffold) {
+        scaffold.blocks.forEach((metalType, block) -> {
+            // Scaffold
+            String scaffoldTexture = metalType.createFullIdWith(GemsRealm.MOD_ID, "block", module.shortenedId(), "scaffold/", "scaffold");
+            String insideTexture = metalType.createFullIdWith(GemsRealm.MOD_ID, "block", module.shortenedId(), "scaffold/", "scaffold_inside");
+            String scaffoldCTM = metalType.createFullIdWith(GemsRealm.MOD_ID, "block", module.shortenedId(), "scaffold/", "scaffold_connected");
+            String insideCTM = metalType.createFullIdWith(GemsRealm.MOD_ID, "block", module.shortenedId(), "scaffold/", "scaffold_inside_connected");
+
+            // Casing
+            String casingTexture = metalType.createFullIdWith(GemsRealm.MOD_ID, "block", module.shortenedId(), "", "casing");
+            String casingCTM = metalType.createFullIdWith(GemsRealm.MOD_ID, "block", module.shortenedId(), "", "casing_connected");
+
+            CTSpriteShiftEntry scaffoldShift = horizontal(scaffoldTexture, scaffoldCTM);
+            CTSpriteShiftEntry scaffoldInsideShift = horizontal(insideTexture, insideCTM);
+            CTSpriteShiftEntry casingShift = omni(casingTexture, casingCTM);
+
+            CreateClient.MODEL_SWAPPER.getCustomBlockModels().register(Utils.getID(block),
+                    model -> new CTModel(model, new MetalScaffoldingCTBehaviour(scaffoldShift, scaffoldInsideShift, casingShift)));
+
+
+        });
+    }
+
+    private static CTSpriteShiftEntry omni(String blockTexture, String connectedTexture) {
+        return getCT(AllCTTypes.OMNIDIRECTIONAL, new ResourceLocation(blockTexture), new ResourceLocation(connectedTexture));
+    }
+
+
+    private static CTSpriteShiftEntry horizontal(String blockTexture, String connectedTexture) {
+        return getCT(AllCTTypes.HORIZONTAL, new ResourceLocation(blockTexture), new ResourceLocation(connectedTexture));
+    }
+
+    private static CTSpriteShiftEntry vertical(String blockTexture, String connectedTexture) {
+        return getCT(AllCTTypes.VERTICAL, new ResourceLocation(blockTexture), new ResourceLocation(connectedTexture));
+    }
+
+}
---
+++ fabric/src/main/java/net/xelbayria/gems_realm/modules/fabric/create/CreateDecoModule.java
@@ -0,0 +1,270 @@
+package net.xelbayria.gems_realm.modules.fabric.create;
+
+import com.simibubi.create.content.decoration.palettes.ConnectedGlassPaneBlock;
+import com.simibubi.create.content.decoration.palettes.WindowBlock;
+import net.fabricmc.api.EnvType;
+import net.fabricmc.api.Environment;
+import net.mehvahdjukaar.every_compat.api.SimpleEntrySet;
+import net.mehvahdjukaar.moonlight.api.util.Utils;
+import net.minecraft.core.registries.Registries;
+import net.minecraft.resources.ResourceLocation;
+import net.minecraft.tags.BlockTags;
+import net.minecraft.world.level.block.Block;
+import net.minecraft.world.level.block.Blocks;
+import net.xelbayria.gems_realm.api.GemsRealmEntrySet;
+import net.xelbayria.gems_realm.api.GemsRealmModule;
+import net.xelbayria.gems_realm.api.set.MetalType;
+import net.xelbayria.gems_realm.api.set.MetalTypeRegistry;
+
+//SUPPORT: v2.0.2
+public class CreateDecoModule extends GemsRealmModule {
+
+    public final SimpleEntrySet<MetalType, Block> window;
+    public final SimpleEntrySet<MetalType, Block> window_pane;
+    public final SimpleEntrySet<MetalType, Block> bars_overlay;
+    public final SimpleEntrySet<MetalType, Block> mesh_fence;
+    public final SimpleEntrySet<MetalType, Block> catwalk;
+    public final SimpleEntrySet<MetalType, Block> catwalk_stairs;
+    public final SimpleEntrySet<MetalType, Block> catwalk_railing;
+    public final SimpleEntrySet<MetalType, Block> support_wedge;
+    public final SimpleEntrySet<MetalType, Block> facade;
+    public final SimpleEntrySet<MetalType, Block> ladder;
+    public final SimpleEntrySet<MetalType, Block> hull;
+    public final SimpleEntrySet<MetalType, Block> support;
+    public final SimpleEntrySet<MetalType, Block> yellow_lamp;
+    public final SimpleEntrySet<MetalType, Block> red_lamp;
+    public final SimpleEntrySet<MetalType, Block> green_lamp;
+    public final SimpleEntrySet<MetalType, Block> blue_lamp;
+    public final SimpleEntrySet<MetalType, Block> sheet_metal;
+
+    public CreateDecoModule(String modId) {
+        super(modId, "cd");
+        ResourceLocation tab = modRes("");
+
+        window = GemsRealmEntrySet.of(MetalType.class, "window",
+                        getModBlock("iron_window"), MetalTypeRegistry::getIronType,
+                        this::makeWindow
+                )
+                .requiresChildren("ingot") //REASON: recipes
+                .addTexture(modRes("block/blanks_1"))
+                .addTag(BlockTags.MINEABLE_WITH_PICKAXE, Registries.BLOCK)
+                .setTabKey(tab)
+                .defaultRecipe()
+                .build();
+        this.addEntry(window);
+
+        window_pane = GemsRealmEntrySet.of(MetalType.class, "window_pane",
+                        getModBlock("iron_window_pane"), MetalTypeRegistry::getIronType,
+                        metalType -> new ConnectedGlassPaneBlock(Utils.copyPropertySafe(Blocks.GLASS_PANE))
+                )
+                .requiresFromMap(window.blocks) //REASON: recipes
+                .addTexture(modRes("block/blanks_2"))
+                .addTag(BlockTags.MINEABLE_WITH_PICKAXE, Registries.BLOCK)
+                .setTabKey(tab)
+                .defaultRecipe()
+                .build();
+        this.addEntry(window_pane);
+
+        bars_overlay = GemsRealmEntrySet.of(MetalType.class, "bars_overlay",
+                        getModBlock("iron_bars_overlay"), MetalTypeRegistry::getIronType,
+                        stoneType -> new Block(Utils.copyPropertySafe(stoneType.block))
+                )
+                //RECIPES: create:sheet
+//                .requiresChildren("create:sheet") //REASON: recipes
+                .addTexture(modRes("block/blanks_3"))
+                .addTag(BlockTags.MINEABLE_WITH_PICKAXE, Registries.BLOCK)
+                .setTabKey(tab)
+                .defaultRecipe()
+                .build();
+        this.addEntry(bars_overlay);
+
+        mesh_fence = GemsRealmEntrySet.of(MetalType.class, "mesh_fence",
+                        getModBlock("iron_mesh_fence"), MetalTypeRegistry::getIronType,
+                        stoneType -> new Block(Utils.copyPropertySafe(stoneType.block))
+                )
+                //RECIPES: create:sheet
+//                .requiresChildren("create:sheet") //REASON: recipes
+                .addTexture(modRes("block/blanks_4"))
+                .addTag(BlockTags.MINEABLE_WITH_PICKAXE, Registries.BLOCK)
+                .setTabKey(tab)
+                .defaultRecipe()
+                .build();
+        this.addEntry(mesh_fence);
+
+        catwalk = GemsRealmEntrySet.of(MetalType.class, "catwalk",
+                        getModBlock("iron_catwalk"), MetalTypeRegistry::getIronType,
+                        stoneType -> new Block(Utils.copyPropertySafe(stoneType.block))
+                )
+                //RECIPES: create:sheet, minecraft:bars
+//                .requiresChildren("create:sheet" /*,"bars"*/) //REASON: recipes
+                .addTexture(modRes("block/blanks_5"))
+                .addTag(BlockTags.MINEABLE_WITH_PICKAXE, Registries.BLOCK)
+                .setTabKey(tab)
+                .defaultRecipe()
+                .build();
+        this.addEntry(catwalk);
+
+        catwalk_stairs = GemsRealmEntrySet.of(MetalType.class, "catwalk_stairs",
+                        getModBlock("iron_catwalk_stairs"), MetalTypeRegistry::getIronType,
+                        stoneType -> new Block(Utils.copyPropertySafe(stoneType.block))
+                )
+                .requiresFromMap(catwalk.blocks) //REASON: recipes
+                .requiresChildren("bars") //REASON: recipes
+                .addTexture(modRes("block/blanks_6"))
+                .addTag(BlockTags.MINEABLE_WITH_PICKAXE, Registries.BLOCK)
+                .setTabKey(tab)
+                .defaultRecipe()
+                .build();
+        this.addEntry(catwalk_stairs);
+
+        catwalk_railing = GemsRealmEntrySet.of(MetalType.class, "catwalk_railing",
+                        getModBlock("iron_catwalk_railing"), MetalTypeRegistry::getIronType,
+                        stoneType -> new Block(Utils.copyPropertySafe(stoneType.block))
+                )
+                //RECIPES: create:sheet, minecraft:bars
+//                .requiresChildren("create:sheet" /*,"bars"*/) //REASON: recipes
+                .addTexture(modRes("block/blanks_7"))
+                .addTag(BlockTags.MINEABLE_WITH_PICKAXE, Registries.BLOCK)
+                .setTabKey(tab)
+                .defaultRecipe()
+                .build();
+        this.addEntry(catwalk_railing);
+
+        support_wedge = GemsRealmEntrySet.of(MetalType.class, "support_wedge",
+                        getModBlock("iron_support_wedge"), MetalTypeRegistry::getIronType,
+                        stoneType -> new Block(Utils.copyPropertySafe(stoneType.block))
+                )
+                //RECIPES: create:sheet
+//                .requiresChildren("create:sheet") //REASON: recipes
+                .addTexture(modRes("block/blanks_8"))
+                .addTag(BlockTags.MINEABLE_WITH_PICKAXE, Registries.BLOCK)
+                .setTabKey(tab)
+                .defaultRecipe()
+                .build();
+        this.addEntry(support_wedge);
+
+        facade = GemsRealmEntrySet.of(MetalType.class, "facade",
+                        getModBlock("iron_facade"), MetalTypeRegistry::getIronType,
+                        stoneType -> new Block(Utils.copyPropertySafe(stoneType.block))
+                )
+                .requiresChildren("ingot") //REASON: recipes
+                .addTexture(modRes("block/blanks_9"))
+                .addTag(BlockTags.MINEABLE_WITH_PICKAXE, Registries.BLOCK)
+                .setTabKey(tab)
+                .defaultRecipe()
+                .build();
+        this.addEntry(facade);
+
+        ladder = GemsRealmEntrySet.of(MetalType.class, "ladder",
+                        getModBlock("iron_ladder"), MetalTypeRegistry::getIronType,
+                        stoneType -> new Block(Utils.copyPropertySafe(stoneType.block))
+                )
+                .requiresChildren("ingot") //REASON: recipes
+                .addTexture(modRes("block/blanks_10"))
+                .addTag(BlockTags.MINEABLE_WITH_PICKAXE, Registries.BLOCK)
+                .setTabKey(tab)
+                .defaultRecipe()
+                .build();
+        this.addEntry(ladder);
+
+        hull = GemsRealmEntrySet.of(MetalType.class, "hull",
+                        getModBlock("iron_hull"), MetalTypeRegistry::getIronType,
+                        stoneType -> new Block(Utils.copyPropertySafe(stoneType.block))
+                )
+                .requiresChildren("create:sheet") //REASON: recipes
+                //RECIPES: block
+                .addTexture(modRes("block/blanks_11"))
+                .addTag(BlockTags.MINEABLE_WITH_PICKAXE, Registries.BLOCK)
+                .setTabKey(tab)
+                .defaultRecipe()
+                .build();
+        this.addEntry(hull);
+
+        support = GemsRealmEntrySet.of(MetalType.class, "support",
+                        getModBlock("iron_support"), MetalTypeRegistry::getIronType,
+                        stoneType -> new Block(Utils.copyPropertySafe(stoneType.block))
+                )
+                .requiresChildren("ingot") //REASON: recipes
+                .addTexture(modRes("block/blanks_12"))
+                .addTag(BlockTags.MINEABLE_WITH_PICKAXE, Registries.BLOCK)
+                .setTabKey(tab)
+                .defaultRecipe()
+                .build();
+        this.addEntry(support);
+
+        yellow_lamp = GemsRealmEntrySet.of(MetalType.class, "lamp", "yellow",
+                        getModBlock("yellow_iron_lamp"), MetalTypeRegistry::getIronType,
+                        stoneType -> new Block(Utils.copyPropertySafe(stoneType.block))
+                )
+                //RECIPES: create:sheet, minecraft:nugget
+                .addTexture(modRes("block/blanks_13"))
+                .addTag(BlockTags.MINEABLE_WITH_PICKAXE, Registries.BLOCK)
+                .setTabKey(tab)
+                .defaultRecipe()
+                .build();
+        this.addEntry(yellow_lamp);
+
+        red_lamp = GemsRealmEntrySet.of(MetalType.class, "lamp", "red",
+                        getModBlock("red_iron_lamp"), MetalTypeRegistry::getIronType,
+                        stoneType -> new Block(Utils.copyPropertySafe(stoneType.block))
+                )
+                //RECIPES: create:sheet, minecraft:nugget
+                .addTexture(modRes("block/blanks_14"))
+                .addTag(BlockTags.MINEABLE_WITH_PICKAXE, Registries.BLOCK)
+                .setTabKey(tab)
+                .defaultRecipe()
+                .build();
+        this.addEntry(red_lamp);
+
+        green_lamp = GemsRealmEntrySet.of(MetalType.class, "lamp", "green",
+                        getModBlock("green_iron_lamp"), MetalTypeRegistry::getIronType,
+                        stoneType -> new Block(Utils.copyPropertySafe(stoneType.block))
+                )
+                //RECIPES: create:sheet, minecraft:nugget
+                .addTexture(modRes("block/blanks_15"))
+                .addTag(BlockTags.MINEABLE_WITH_PICKAXE, Registries.BLOCK)
+                .setTabKey(tab)
+                .defaultRecipe()
+                .build();
+        this.addEntry(green_lamp);
+
+        blue_lamp = GemsRealmEntrySet.of(MetalType.class, "lamp", "blue",
+                        getModBlock("blue_iron_lamp"), MetalTypeRegistry::getIronType,
+                        stoneType -> new Block(Utils.copyPropertySafe(stoneType.block))
+                )
+                //RECIPES: create:sheet, minecraft:nugget
+                .addTexture(modRes("block/blanks_16"))
+                .addTag(BlockTags.MINEABLE_WITH_PICKAXE, Registries.BLOCK)
+                .setTabKey(tab)
+                .defaultRecipe()
+                .build();
+        this.addEntry(blue_lamp);
+
+        sheet_metal = GemsRealmEntrySet.of(MetalType.class, "sheet_metal",
+                        getModBlock("iron_sheet_metal"), MetalTypeRegistry::getIronType,
+                        stoneType -> new Block(Utils.copyPropertySafe(stoneType.block))
+                )
+                //RECIPES: create:sheet
+                .addTexture(modRes("block/blanks_17"))
+                .addTag(BlockTags.MINEABLE_WITH_PICKAXE, Registries.BLOCK)
+                .setTabKey(tab)
+                .defaultRecipe()
+                .build();
+        this.addEntry(sheet_metal);
+
+    }
+
+    private WindowBlock makeWindow(MetalType metalType) {
+        return new WindowBlock(Utils.copyPropertySafe(Blocks.GLASS)
+                .isValidSpawn((s, l, ps, t) -> false).isRedstoneConductor((s, l, ps) -> false)
+                .isSuffocating((s, l, ps) -> false).isViewBlocking((s, l, ps) -> false), false);
+    }
+
+    @Override
+    @Environment(EnvType.CLIENT)
+    public void onClientSetup() {
+        super.onClientSetup();
+        CreateClientModule.registerWindowCTBehavior(this, window, window_pane);
+    }
+
+}
