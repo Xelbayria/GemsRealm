934f6b2cd947405304662632a3f58632f25fad93
Xel'Bayria <12866666+Xelbayria@users.noreply.github.com>
2025-08-15 22:50:46 -0600

    NEW CLASS: VanillaRockChildKeys

---
+++ common/src/main/java/net/xelbayria/gems_realm/api/set/VanillaRockChildKeys.java
@@ -0,0 +1,29 @@
+package net.xelbayria.gems_realm.api.set;
+
+@SuppressWarnings("unused")
+public class VanillaRockChildKeys {
+
+    public static final String BLOCK = "block";
+    public static final String RAW_BLOCK = "raw_block";
+    public static final String STAIRS = "stairs";
+    public static final String SLAB = "slab";
+    public static final String FENCE = "fence";
+    public static final String CHISELED = "chiseled";
+
+    public static final String BRICKS = "bricks";
+    public static final String BRICK_STAIRS = "brick_stairs";
+    public static final String BRICK_SLAB = "brick_slab";
+    public static final String BRICK_WALL = "brick_wall";
+    public static final String BRICK_TILES = "brick_tiles";
+    public static final String CRACKED_BRICKS = "cracked_bricks";
+
+    public static final String MOSSY_BRICKS = "mossy_bricks";
+    public static final String MOSSY_BRICK_SLAB = "mossy_brick_slab";
+    public static final String MOSSY_BRICK_STAIRS = "mossy_brick_stairs";
+    public static final String MOSSY_BRICK_WALL = "mossy_brick_wall";
+
+    public static final String SMOOTH = "smooth";
+    public static final String SMOOTH_STAIRS = "smooth_stairs";
+    public static final String SMOOTH_SLAB = "smooth_slab";
+    public static final String SMOOTH_WALL = "smooth_wall";
+}
