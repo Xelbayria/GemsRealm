94fca8c6e067eddf195bbaea74f5963aa1dc2486
Xel'Bayria <12866666+Xelbayria@users.noreply.github.com>
2025-09-19 00:07:33 -0600

    NeW CLASSES: MetalPaletteStrategies

---
+++ common/src/main/java/net/xelbayria/gems_realm/api/MetalPaletteStrategies.java
@@ -0,0 +1,11 @@
+package net.xelbayria.gems_realm.api;
+
+import net.mehvahdjukaar.every_compat.api.PaletteStrategies;
+import net.mehvahdjukaar.every_compat.api.PaletteStrategy;
+import net.xelbayria.gems_realm.api.set.metal.VanillaMetalChildKeys;
+
+public class MetalPaletteStrategies extends PaletteStrategies {
+
+    public static final PaletteStrategy TRAPDOOR_STANDARD = registerCached((blockType, manager) -> PaletteStrategies.makePaletteFromChild(
+            blockType, manager, VanillaMetalChildKeys.TRAPDOOR, null, null));
+}
