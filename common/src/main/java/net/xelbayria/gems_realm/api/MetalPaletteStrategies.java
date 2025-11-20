44a08a40edb76c70b18dbef084eb7ab41c1a5200
Xel'Bayria <12866666+Xelbayria@users.noreply.github.com>
2025-11-20 03:33:31 -0700

    MetalPaletteStrategies: Added RAW_BLOCK_STANDARD

--- common/src/main/java/net/xelbayria/gems_realm/api/MetalPaletteStrategies.java
+++ common/src/main/java/net/xelbayria/gems_realm/api/MetalPaletteStrategies.java
@@ -2,13 +2,19 @@ package net.xelbayria.gems_realm.api;
 
 import net.mehvahdjukaar.every_compat.api.PaletteStrategies;
 import net.mehvahdjukaar.every_compat.api.PaletteStrategy;
+import net.xelbayria.gems_realm.api.set.VanillaRockChildKeys;
 import net.xelbayria.gems_realm.api.set.metal.VanillaMetalChildKeys;
 
 public class MetalPaletteStrategies extends PaletteStrategies {
 
+// ────────────────────────────────────────────────────── Blocks ─────────────────────────────────────────────────────--
+    public static final PaletteStrategy RAW_BLOCK_STANDARD = registerCached((blockType, manager) -> PaletteStrategies.makePaletteFromChild(
+            blockType, manager, VanillaRockChildKeys.RAW_BLOCK, null, null));
+
     public static final PaletteStrategy TRAPDOOR_STANDARD = registerCached((blockType, manager) -> PaletteStrategies.makePaletteFromChild(
             blockType, manager, VanillaMetalChildKeys.TRAPDOOR, null, null));
 
+// ─────────────────────────────────────────────────────── Items ──────────────────────────────────────────────────────-
     public static final PaletteStrategy INGOT_STANDARD = registerCached((blockType, manager) -> PaletteStrategies.makePaletteFromChild(
             blockType, manager, VanillaMetalChildKeys.INGOT, null, null));
 }
