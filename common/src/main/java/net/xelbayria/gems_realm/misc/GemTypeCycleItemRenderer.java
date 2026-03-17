a89d0ada7708f61a044e2310099693e49836a51a
Xel'Bayria <12866666+Xelbayria@users.noreply.github.com>
2026-03-17 03:57:33 -0600

    GRConfigs: Removed 2 configs for CYCLE_ITEM_RENDERER

--- common/src/main/java/net/xelbayria/gems_realm/misc/GemTypeCycleItemRenderer.java
+++ common/src/main/java/net/xelbayria/gems_realm/misc/GemTypeCycleItemRenderer.java
@@ -1,14 +1,7 @@
 package net.xelbayria.gems_realm.misc;
 
 import net.mehvahdjukaar.every_compat.misc.BlockTypeCycleItemRenderer;
-import net.minecraft.core.registries.BuiltInRegistries;
-import net.minecraft.resources.ResourceLocation;
-import net.minecraft.world.item.ItemStack;
-import net.minecraft.world.item.Items;
 import net.xelbayria.gems_realm.api.set.metal.MetalType;
-import net.xelbayria.gems_realm.configs.GRConfigs;
-
-import java.util.Objects;
 
 public class GemTypeCycleItemRenderer extends BlockTypeCycleItemRenderer<MetalType> {
 
@@ -16,18 +9,4 @@ public class GemTypeCycleItemRenderer extends BlockTypeCycleItemRenderer<MetalTy
         super(MetalType.class);
     }
 
-    @Override
-    @SuppressWarnings("ConstantValue")
-    public ItemStack getItemIcon() {
-        var itemId = GRConfigs.CREATIVE_TAB_ICON.get();
-        ItemStack item = BuiltInRegistries.ITEM.get(ResourceLocation.parse(itemId)).getDefaultInstance();
-
-        if (Objects.nonNull(item)) return item;
-        else return Items.BARRIER.getDefaultInstance();
-    }
-
-    @Override
-    public boolean getDisableCycleItemRenderer() {
-        return GRConfigs.DISABLE_CYCLE_ITEM_RENDERER.get();
-    }
 }
